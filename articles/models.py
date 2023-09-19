536df88600e60d4e1a5515532c9fab5356a52127
Aster696 <aster.r@codezen.in>
2023-09-19 21:45:54 +0530

    first commoit

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Articles(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    # add in thumnail
+    # add in author later
+
+    def __str__(self):
+        return self.title
+    
+    def snippets(self):
+        if len(self.body) > 50:
+            return self.body[:50] + '...'
+        else:
+            return self.body
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Articles
 
 def article_list(request): 
-    return render(request, 'articles/articles_list.html')
+    articles = Articles.objects.all().order_by('date')
+    return render(request, 'articles/articles_list.html', {'articles': articles})
