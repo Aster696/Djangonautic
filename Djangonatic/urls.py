ec825024e28a0fa75ef5b6216ad8d4ae294847b7
Aster696 <aster.r@codezen.in>
2023-09-19 09:35:00 +0530

    first commit

--- Djangonatic/urls.py
+++ Djangonatic/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homePage),
     path('about', views.aboutPage),
+    path('articles/', include('articles.urls')),
 ]
