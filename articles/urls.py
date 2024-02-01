70cc3659bb2e908fba25ff93061cb6c141f41e28
Aster696 <aster.r@codezen.in>
2024-02-01 08:22:50 +0530

    First c

--- articles/urls.py
+++ articles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.article_list)
+    path('', views.article_list),
+    path('/<int: param>/', views.article_details)
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Articles
 
 def article_list(request): 
     articles = Articles.objects.all().order_by('date')
     return render(request, 'articles/articles_list.html', {'articles': articles})
+
+def article_details(request, param):
+    return HttpResponse('Details')
